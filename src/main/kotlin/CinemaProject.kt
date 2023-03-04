0a7ff3803bc4dc37d4fb3a5687e552cb1f923be5
Vladimir Makarov <makarov86@gmail.com>
2023-03-04 16:35:56 +0300

    Cinema Stage 3/5 done!

--- src/main/kotlin/CinemaProject.kt
+++ src/main/kotlin/CinemaProject.kt
@@ -7,51 +7,73 @@ const val HIGH_PRICE = 10
 const val LOW_PRICE = 8
 
 fun main() {
-    println("Enter the number of rows:\n> ")
-    print("> ")
-    val rowsCount = readln().toInt()
+    // Любой ввод вывод или пользовательский интерфейс
+    // должен быть изолирован и абстрагирован от остальной логики
+    val screen = ConsoleTerminalScreen()
 
-    println("Enter the number of seats in each row:\n> ")
-    print("> ")
-    val seatsCount = readln().toInt()
+    screen.print("Enter the number of rows:")
+    val rowsCount = screen.readInt()
+
+    screen.print("Enter the number of seats in each row:")
+    val seatsCount = screen.readInt()
 
     // Создаем экземпляр объекта кинотеатра
-    val cinema = Cinema(rowsCount, seatsCount)
+    val cinema = VidnoeCinema(rowsCount, seatsCount)
 
-    val ticketOffice = TicketOffice(cinema)
+    // Создаем экземпляр объекта кассы кинотеатров
+    val ticketOffice = TicketOffice(cinema, screen)
 
-    val income: Int
+    screen.print("\nCinema:")
+    screen.print(cinema.schemeToString())
 
-    if (rowsCount * seatsCount <= TOTAL_SEATS_EQUAL_PRICE_LIMIT) {
-        income = rowsCount * seatsCount * HIGH_PRICE
-    } else {
-        val highPriceRowsCount = rowsCount / 2
-        val lowPriceRowCount = rowsCount - highPriceRowsCount
-        income = highPriceRowsCount * seatsCount * HIGH_PRICE + lowPriceRowCount * seatsCount * LOW_PRICE
-    }
-
-    println("Total income:")
-    println("\$$income")
+    screen.print("Enter a row number:")
+    val rowNumber = screen.readInt()
+    screen.print("Enter a seat number in that row:")
+    val seatNumber = screen.readInt()
+    screen.print("\nTicket price: \$${cinema.getTicketPrice(rowNumber, seatNumber)}\n")
+    cinema.sellSeat(rowNumber, seatNumber)
+    screen.print("Cinema:")
+    screen.print(cinema.schemeToString())
 }
 
 /**
- * Класс кинотеатр
+ * Интерфейс для конкретных кинотеатров
+ */
+interface Cinema {
+    /**
+     * Получить цену билета на конкретный ряд и место
+     */
+    fun getTicketPrice(rowNumber: Int, seatNumber: Int) : Int
+
+    /**
+     * Продать место
+     */
+    fun sellSeat(rowNumber: Int, seatNumber: Int)
+
+    /**
+     * Вернет схему зала в виде строки
+     */
+    fun schemeToString() : String
+}
+
+/**
+ * Класс видновский кинотеатр
  * @param rowsCount Кол-во рядов
  * @param seatsCount Кол-во мест в одном ряду
  * @constructor Создать объект кинотеатр
  */
-class Cinema (rowsCount: Int, seatsCount: Int) {
+class VidnoeCinema (rowsCount: Int, seatsCount: Int) : Cinema {
 
-    private val rowsCount: Int
-    private val seatsCount: Int
+    private val _rowsCount: Int
+    private val _seatsCount: Int
 
     /**
      * Признак того что кинотеатр маленький
      */
-    private val isSmallCinema: Boolean
+    private val _isSmallCinema: Boolean
 
     /**
-     * Схема зала с местами (MutableList нам избыточен, схема не будет менять в процессе работы)
+     * Схема зала с местами (MutableList нам избыточен, схема не будет меняться в процессе работы)
      */
     private val _seatsScheme: List<List<Seat>>
 
@@ -60,15 +82,15 @@ class Cinema (rowsCount: Int, seatsCount: Int) {
             throw Exception("Error!")
         }
 
-        this.rowsCount = rowsCount
-        this.seatsCount = seatsCount
+        this._rowsCount = rowsCount
+        this._seatsCount = seatsCount
 
         // Инициализация схемы зала, список списков мест, и элементы списка - объекты класса Место
         _seatsScheme = List<List<Seat>>(rowsCount) {
             List<Seat>(seatsCount) { Seat() }
         }
 
-        isSmallCinema = rowsCount * seatsCount <= TOTAL_SEATS_EQUAL_PRICE_LIMIT
+        _isSmallCinema = rowsCount * seatsCount <= TOTAL_SEATS_EQUAL_PRICE_LIMIT
 
         // Теперь для схемы зала нужно просчитать и заполнить цены на места
         for (rowInd in _seatsScheme.indices) {
@@ -78,16 +100,44 @@ class Cinema (rowsCount: Int, seatsCount: Int) {
         }
     }
 
-    public fun getTicketPrice(rowNumber: Int, seatNumber: Int) : Int {
+    /**
+     * Получить цену билета на конкретный ряд и место
+     */
+    public override fun getTicketPrice(rowNumber: Int, seatNumber: Int) : Int {
         // Работает со схемой зала, в ней уже заранее просчитаны все цены
         return _seatsScheme[rowNumber - 1][seatNumber - 1].price!!
     }
 
     /**
+     * Продать место
+     */
+    public override fun sellSeat(rowNumber: Int, seatNumber: Int) {
+        // TODO проверить приделы
+        val seat = _seatsScheme[rowNumber - 1][seatNumber - 1]
+
+        if (!seat.available) throw Exception("Already sold")
+        seat.available = false
+    }
+
+    /**
      * Вернет схему зала в виде строки
      */
-    override fun toString(): String {
-        return super.toString()
+    public override fun schemeToString(): String {
+        val strBuilder = StringBuilder()
+        strBuilder.append("  ")
+        for (seat in 1.._seatsCount){
+            strBuilder.append("$seat ")
+        }
+        strBuilder.appendLine()
+        for (row in 1.._rowsCount){
+            strBuilder.append("$row ")
+            for (seat in 1.._seatsCount){
+                strBuilder.append(_seatsScheme[row - 1][seat - 1].toString() + " ")
+            }
+            strBuilder.appendLine()
+        }
+
+        return strBuilder.toString()
     }
 
     /**
@@ -95,10 +145,10 @@ class Cinema (rowsCount: Int, seatsCount: Int) {
      * @param rowNumber номер ряда
      */
     private fun calcPrice(rowNumber: Int): Int {
-        if (isSmallCinema) {
+        if (_isSmallCinema) {
             return HIGH_PRICE
         }
-        val highPriceRowsCount = rowsCount / 2
+        val highPriceRowsCount = _rowsCount / 2
         return if (rowNumber <= highPriceRowsCount) HIGH_PRICE else LOW_PRICE
     }
 }
@@ -124,9 +174,39 @@ class Seat(available: Boolean = true) {
 }
 
 /**
- * Класс кассы кинотеатров
+ * Интерфейс экрана терминала
  */
-class TicketOffice(cinema: Cinema){
+interface TerminalScreen {
+    fun print(str: String)
+    fun readInt() : Int
+}
+
+/**
+ * Реализация терминала - ввода вывода пользователя на консоль приложения
+ */
+class ConsoleTerminalScreen : TerminalScreen {
+    public override fun print(str: String) {
+        println(str)
+    }
+
+    public override fun readInt(): Int {
+        var value: Int?
+        do {
+            System.out.print("> ")
+            value = readln().toIntOrNull()
+        } while (value == null)
+
+        return value
+    }
+}
+
+/**
+ * Класс кассы кинотеатров.
+ * При инициализации объектов класса - используются абстракции кинотеатра и экрана терминала, (через интерфейсы)
+ * @param cinema объект реализующий интерфейс Кинотеатр
+ * @param terminalScreen объект реализующий интерфейс экрана терминала
+ */
+class TicketOffice(cinema: Cinema, terminalScreen: TerminalScreen){
     val cinema: Cinema
 
     init {
