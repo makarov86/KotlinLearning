c31a1f554146ae3705426ce136502b50f965955c
Vladimir Makarov <makarov86@gmail.com>
2023-03-07 15:28:45 +0300

    Parking lot. Stage 4/5

--- src/main/kotlin/ParkingLot.kt
+++ src/main/kotlin/ParkingLot.kt
@@ -1,10 +1,8 @@
 package parking
 
-const val SLOT_COUNT = 20
-
 fun main() {
     val terminal = ConsoleParkTerminal()
-    val spots = List<Spot>(SLOT_COUNT) { Spot() }
+    var spots = mutableListOf<Spot>()
 
     while (true) {
         val cmd: Command
@@ -18,12 +16,54 @@ fun main() {
         when (cmd.type) {
             CommandType.Park -> park(terminal, spots, cmd.carNumber!!, cmd.carColor!!)
             CommandType.Leave -> leave(terminal, spots, cmd.spotNumber!!)
+            CommandType.Create -> create(terminal, spots, cmd.spotCount!!)
+            CommandType.Status -> showStatus(terminal, spots)
             CommandType.Exit -> return
         }
     }
 }
 
+fun showStatus(terminal: TerminalScreen, spots: List<Spot>) {
+    if (spots.isEmpty()) {
+        terminal.print("Sorry, a parking lot has not been created.")
+        return
+    }
+
+    var hasAnyCar = false
+    for (spot in spots) {
+        if (spot.car != null) {
+            hasAnyCar = true
+            break
+        }
+    }
+
+    if (!hasAnyCar) {
+        terminal.print("Parking lot is empty.")
+        return
+    }
+
+    for (spotIndex in spots.indices) {
+        val spotNumber = spotIndex + 1
+        val spot = spots[spotIndex]
+        if (spot.car != null)
+        {
+            terminal.print("$spotNumber ${spot.car?.number} ${spot.car?.color}")
+        }
+    }
+}
+
+fun create(terminal: TerminalScreen, spots: MutableList<Spot>, spotCount: Int) {
+    spots.clear()
+    spots.addAll(List<Spot>(spotCount) { Spot() })
+    terminal.print("Created a parking lot with $spotCount spots.")
+}
+
 fun leave(terminal: TerminalScreen, spots: List<Spot>, spotNumber: Int) {
+    if (spots.isEmpty()) {
+        terminal.print("Sorry, a parking lot has not been created.")
+        return
+    }
+
     if (spotNumber - 1 > spots.lastIndex) {
         terminal.print("Error")
         return
@@ -40,6 +80,11 @@ fun leave(terminal: TerminalScreen, spots: List<Spot>, spotNumber: Int) {
 }
 
 fun park(terminal: TerminalScreen, spots: List<Spot>, carNumber: String, carColor: String) {
+    if (spots.isEmpty()) {
+        terminal.print("Sorry, a parking lot has not been created.")
+        return
+    }
+
     var freeSpotIndex: Int? = null
     for (i in spots.indices) {
         if (spots[i].isFree()) {
@@ -53,9 +98,10 @@ fun park(terminal: TerminalScreen, spots: List<Spot>, carNumber: String, carColo
         return
     }
 
-    spots[freeSpotIndex].car = Car(carNumber, carColor)
+    val car = Car(carNumber, carColor)
+    spots[freeSpotIndex].car = car
 
-    terminal.print("$carColor car parked in spot ${freeSpotIndex + 1}.")
+    terminal.print("${car.color} car parked in spot ${freeSpotIndex + 1}.")
 }
 
 fun parseCmd(str: String) : Command {
@@ -63,16 +109,22 @@ fun parseCmd(str: String) : Command {
     if (cmd.isBlank() || cmd.isEmpty()) throw IllegalArgumentException()
     val cmdParts = cmd.replace(">", "").trim().split(' ')
 
-    if (cmdParts.size == 1 && cmdParts[0] == CommandType.Exit.cmdTypeName) {
-        return Command(CommandType.Exit)
+    if (cmdParts.size == 1) {
+        if (cmdParts[0] == CommandType.Exit.cmdTypeName) return Command(CommandType.Exit)
+        if (cmdParts[0] == CommandType.Status.cmdTypeName) return Command(CommandType.Status)
     }
 
     if (cmdParts.size < 2) throw IllegalArgumentException()
 
     when (cmdParts[0]) {
+        CommandType.Create.cmdTypeName -> {
+            if (cmdParts.size != 2) throw IllegalArgumentException()
+            val spotCount = cmdParts[1].toIntOrNull() ?: throw IllegalArgumentException()
+            return Command(type = CommandType.Create, spotCount = spotCount!!)
+        }
         CommandType.Park.cmdTypeName -> {
-            if (cmdParts.size < 3) throw IllegalArgumentException()
-            val number = cmdParts.subList(1, cmdParts.lastIndex - 1).joinToString()
+            if (cmdParts.size != 3) throw IllegalArgumentException()
+            val number = cmdParts[1]
             var color = cmdParts.last()
             return Command(type = CommandType.Park, carNumber = number, carColor = color)
         }
@@ -85,13 +137,21 @@ fun parseCmd(str: String) : Command {
     }
 }
 
-enum class CommandType(val cmdTypeName: String) { Park("park"), Leave("leave"), Exit("exit") }
+enum class CommandType(val cmdTypeName: String) {
+    Create("create"),
+    Park("park"),
+    Leave("leave"),
+    Status("status"),
+    Exit("exit")
+}
 
 data class Command(
     val type: CommandType,
     val carNumber: String? = null,
     val carColor: String? = null,
-    val spotNumber: Int? = null)
+    val spotNumber: Int? = null,
+    val spotCount: Int? = null
+)
 
 class Spot(var car: Car? = null){
     public fun isFree(): Boolean = car == null
